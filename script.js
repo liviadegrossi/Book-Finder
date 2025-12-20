2c2feead99ee4534b0ee4f68ca7f9dc869b11d25
Livia Degrossi <liviadegrossi@gmail.com>
2025-12-20 16:07:23 +0100

    Add footer, retrieve API key and update style

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // AmazonAPI https://webservices.amazon.com/paapi5/documentation/
+// GoogleAPI 
+const searchForm = document.getElementById('searchForm');
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // to prevent the browser to navigate away
+   openFile();
+});
+
+async function openFile() {
+
+    const response = await fetch('GoogleAPI.json', {cache: 'no-store'});
+
+    if(!response.ok) throw new Error('Could not open the file.', response.status);
+    const API_KEY = await response.json();
+    console.log(API_KEY.API_KEY);
+}
